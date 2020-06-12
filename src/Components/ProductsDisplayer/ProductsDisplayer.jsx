ccfd26f12bd46c75308019e065cbfd69132241a6
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-06-12 18:48:42 +0500

    Products demo tested

--- src/Components/ProductsDisplayer/ProductsDisplayer.jsx
+++ src/Components/ProductsDisplayer/ProductsDisplayer.jsx
@@ -4,12 +4,24 @@ export class ProductsDisplayer extends Component {
     render() {
         return (
             <div className="product_displayer_cont flex">
-                <div className="product_displayer_box"></div>
-                <div className="product_displayer_box"></div>
-                <div className="product_displayer_box"></div>
-                <div className="product_displayer_box"></div>
-                <div className="product_displayer_box"></div>
-                <div className="product_displayer_box"></div>
+                <div className="product_displayer_box">
+                    <div className="test"></div>
+                </div>
+                <div className="product_displayer_box">
+                    <div className="test"></div>
+                </div>
+                <div className="product_displayer_box">
+                    <div className="test"></div>
+                </div>
+                <div className="product_displayer_box">
+                    <div className="test"></div>
+                </div>
+                <div className="product_displayer_box">
+                    <div className="test"></div>
+                </div>
+                <div className="product_displayer_box">
+                    <div className="test"></div>
+                </div>
             </div>
         )
     }
