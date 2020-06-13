ecfc592b0b146aeee4e2f5adb676f2d521a9aca7
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-06-13 22:19:05 +0500

    Fetching all products from db done 100%

--- src/Components/ProductsDisplayer/ProductsDisplayer.jsx
+++ src/Components/ProductsDisplayer/ProductsDisplayer.jsx
@@ -12,9 +12,12 @@ export class ProductsDisplayer extends Component {
                 break;
             case 'mentshirts':
                 this.displaytShirtsMen()
+                break;    
+            case 'womentshirts':
+                this.displaytShirtsWomen()
                 break;              
             default:
-                break;
+                this.props.history.push('/');
         }   
     }
     displayShoes = async() =>{
@@ -84,6 +87,41 @@ export class ProductsDisplayer extends Component {
         } catch (error) {
             
         }
+    }
+
+
+    displaytShirtsWomen = async() =>{
+                //fetch div
+                var product_displayer_cont = document.querySelector('.product_displayer_cont');
+                //fetch images and products info
+                try {
+                    var check = await firebaseStorage.ref('t-shirts(women)/').listAll();
+                    //get image through download url
+                    check.items.forEach( element => {
+                        var name = element.name.split('.')[0];
+                        element.getDownloadURL().then(async(url)=>{
+                            //fetch price
+                            var productObj = await firestore.collection('t-shirts(women)').doc(name).get();
+                            //display products
+                            product_displayer_cont.insertAdjacentHTML('beforeend',
+                            `<div class="product_displayer_box pointer" id="${name}"  >
+                                <div class="product_image">
+                                    <img class="product_image_setting" src=${url} alt="p"/>
+                                </div>
+                                <div class="product_price flex-col">
+                                    <h1 class="para2">${name}</h1>
+                                    <h1 class="para2 lighter">${productObj.data().price}$</h1>
+                                </div>
+                            </div>`)
+                            //set an on click function for all the products
+                            document.querySelector("#"+name).addEventListener('click',()=>{
+                                console.log(name)
+                            })
+                        })
+                    });
+                } catch (error) {
+                    
+                }
     }
 
 
