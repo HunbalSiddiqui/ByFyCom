3bc0f69f666da523cccafaf48118f0964f580610
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-06-15 01:57:22 +0500

    LowLevel Security done

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -2,24 +2,35 @@ import React from 'react'
 import './CheckOut.css'
 import HomeNav from '../../Components/HomeNav/HomeNav'
 import CheckOutTable from '../../Components/CheckOutTable/CheckOutTable'
-function CheckOut() {
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+function CheckOut(props) {
     return (
-        <div className="checkOut_cont">
-            <div className="checkOut_wrapper">
-                <div className="checkOut_nav_area">
-                    <HomeNav/>
+        props.currentUser ?
+            <div className="checkOut_cont">
+                <div className="checkOut_wrapper">
+                    <div className="checkOut_nav_area">
+                        <HomeNav/>
+                    </div>
+                    <div className="checkOut_headings flex-col">
+                        <h1 className="heading2">Here's what you are getting.</h1>
+                        <h1 className="para1 lighter">Make sure to check the total price before confirming your order.</h1>
+                    </div>
+                    <div className="shopping_bag">
+                        <CheckOutTable/>    
+                    </div>
+                    <div className="checkOut_footer"></div>
                 </div>
-                <div className="checkOut_headings flex-col">
-                    <h1 className="heading2">Here's what you are getting.</h1>
-                    <h1 className="para1 lighter">Make sure to check the total price before confirming your order.</h1>
-                </div>
-                <div className="shopping_bag">
-                    <CheckOutTable/>    
-                </div>
-                <div className="checkOut_footer"></div>
             </div>
-        </div>
+            :
+            <Redirect to={'/'} />
     )
 }
 
-export default CheckOut
+var mapStateToProps=(state)=>{
+    return{
+        currentUser:state.userReducer.userObj
+    }
+}
+
+export default connect(mapStateToProps)(CheckOut)
